64bf538965065756da2ab3343393e29332ec6483
Nail Gafarov <nil20122013@gmail.com>
2021-10-14 11:48:41 +0500

    Added documentation to ObjectMap

--- src/ObjectMap.ts
+++ src/ObjectMap.ts
@@ -1,1 +1,5 @@
+/**
+ * Shorthand for mapped types `{[key: string]: <here is type of values>}`.
+ * @typeParam T - Type of objects that the current object store.
+ */
 export type ObjectMap<T> = {[K: string]: T};
