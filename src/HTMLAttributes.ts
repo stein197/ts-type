44ab1bebd70ad3154a7e7f54b0cd8991ed1ffdd7
Nail Gafarov <nil20122013@gmail.com>
2022-09-17 21:01:44 +0500

    Basic attributes setup

---
+++ src/HTMLAttributes.ts
@@ -0,0 +1,19 @@
+// TODO: Take everything from https://html.spec.whatwg.org/ instead of https://developer.mozilla.org/en
+// TODO: Add documentation per each entry
+
+export type ElementAttributesMap = HTMLElementAttributesMap & SVGElementAttributesMap & MathMLElementAttributesMap;
+
+// TODO
+export type HTMLElementAttributesMap = {}
+
+// TODO
+export type SVGElementAttributesMap = {}
+
+// TODO
+export type MathMLElementAttributesMap = {}
+
+// TODO
+type GlobalAttributes = AriaAttributes & {}
+
+// TODO
+type AriaAttributes = {}
